da41c6a4ebcc563f4263be7a281076b8446e1cb6
bjdmayor <bjdmayor@gmail.com>
2023-02-22 05:30:56 -0800

    2-print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,18 +6,20 @@
  */
 
 void print_alphabet_x10(void)
-        
-{       
-        char c;
-        int i = 0;
-        
-        while (i <= 0)
-        {       
-        for (c = 'a'; c <= 'z'; c++)
-	{       
-        _putchar(c);
+
+{
+
+	char c;
+	int i = 0;
+
+	while (i <= 0)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
 	}
-        _putchar('\n');
-        i++;
+	_putchar('\n');
+	i++;
 	}
-}             
+
+}
